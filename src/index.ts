204061eaf68b22ff0664ea40d9ad58deca829f04
evofan <evofan@outlook.jp>
2020-09-23 00:02:57 +0900

    add chap19 State pattern sample

--- src/index.ts
+++ src/index.ts
@@ -382,3 +382,220 @@ let m4 = new MainFactory4();
 m4.main("山田太郎");
 m4.main("鈴木花子");
 console.log("");
+
+
+console.log("\n■ 19 ■ Stateパターン、状態によって遷移、ゲームでもよく使われる");
+
+// サンプルでは1つの状態を1つのクラスで表現するようになっている、インターフェースで必要なメソッドを保証
+
+// Stateインターフェース、状態に応じて呼び出されるインターフェース（API）を規定
+// 状態依存メソッドの集まり
+
+// 「分割して統治せよ（divide and conquer）」...大きい問題は解決出来る小さい問題に分けて考える、状態パターンもその一つ
+
+
+interface IFState {
+    doClock(hour: number): void;
+    doUse(): void;
+    doAlbam(): void;
+    doPhone(): void;
+}
+
+class DayState implements IFState {
+    private DayState() {
+        //
+    }
+    // Singleton
+    private static _instance: any;
+    public static getInstance() {
+        if (!this._instance) {
+            this._instance = new DayState(DayState.getInstance);
+        }
+        return this._instance;
+    }
+    private constructor(caller: Function) {
+        if (caller === DayState.getInstance) {
+            console.log("昼間インスタンス新規作成");
+        } else if (DayState._instance) {
+            throw new Error("DayState既にインスタンスが存在するためエラー。");
+        } else {
+            throw new Error("DayStateコンストラクタの引数が不正な為エラー。");
+        }
+    }
+    public doClock(hour: number): void {
+        console.log("入力値は、" + hour);
+        if (hour < 9 || hour >= 17) {
+            console.log("夜間モードに変更");
+            safeFrame.changeState(NightState.getInstance())
+        } else {
+            console.log("昼間モードのままです");
+        }
+    };
+    public doUse(): void {
+        safeFrame.recordLog("金庫使用（昼間）");
+    };
+    public doAlbam(): void {
+        safeFrame.callSecurityCenter("非常ベル（昼間）");
+    };
+    public doPhone(): void {
+        safeFrame.callSecurityCenter("通常の通話（昼間）");
+    };
+    public toString(): string {
+        return "[昼間]"
+    }
+
+}
+
+class NightState implements IFState {
+    private NightState() {
+        //
+    }
+    // Singleton
+    private static _instance: any;
+    public static getInstance() {
+        if (!this._instance) {
+            this._instance = new NightState(NightState.getInstance);
+        }
+        return this._instance;
+    }
+    private constructor(caller: Function) {
+        if (caller === NightState.getInstance) {
+            console.log("夜間インスタンス新規作成");
+        } else if (NightState._instance) {
+            throw new Error("既にNightStateインスタンスが存在するためエラー。");
+        } else {
+            throw new Error("NightStateコンストラクタの引数が不正な為エラー。");
+        }
+    }
+    public doClock(hour: number): void {
+        console.log("入力値は、" + hour);
+        if (hour >= 9 && hour < 17) {
+            console.log("昼間モードに変更");
+            safeFrame.changeState(DayState.getInstance())
+        } else {
+            console.log("夜間モードのままです");
+        }
+    };
+    public doUse(): void {
+        safeFrame.recordLog("非常：夜間の金庫使用！");
+    };
+    public doAlbam(): void {
+        safeFrame.callSecurityCenter("非常ベル（夜間）");
+    };
+    public doPhone(): void {
+        safeFrame.callSecurityCenter("夜間の通話録音（夜間）");
+    };
+    public toString(): string {
+        return "[夜間]"
+    }
+
+}
+
+// Contextインターフェース、状態を管理する
+interface IFContext {
+    setClock(): void; // 時刻の設定
+    changeState(state: any): void; // 状態を変化させる
+    callSecurityCenter(msg: string): void; // 警備センターを呼び出す
+    recordLog(msg: string): void; // 記録する
+}
+
+//SafeFrameクラス、Safe=金庫
+class SafeFrame implements IFContext {
+
+    private state: any = DayState.getInstance();
+
+    // constractor
+    public SefeFrame(title: string) {
+        console.log("this.state:" + this.state);
+    }
+
+    // ボタン押下時の処理
+    public pushButton(n: number) {
+        console.log("n: " + n);
+        console.log("this.state:" + this.state);
+        switch (n) {
+            case 1:
+                console.log("ボタン1");
+                this.state.doUse();
+                break;
+            case 2:
+                console.log("ボタン2");
+                this.state.doAlbam();
+                break;
+            case 3:
+                console.log("ボタン3");
+                this.state.doPhone();
+                break;
+            case 4:
+                console.log("ボタン4、終了しました");
+                if (safeFrame) { delete safeFrame.state };
+                throw new Error('システムは終了しました');
+                break;
+            default:
+                console.log("error num: " + n);
+                break;
+        }
+    }
+
+    // 時刻設定
+    public setClock() {
+        console.log("setClock()");
+        let t: number = Number($("#hour_input").val());
+        console.log("現在の時間は、" + t);
+        this.state.doClock(t);
+    }
+
+    // ステート変更
+    public changeState(state: any) {
+        console.log("changeState()");
+        console.log(`${this.state}から${state}に状態が変化しました`);
+        this.state = state;
+    }
+
+    // 警備センター呼び出し
+    public callSecurityCenter(msg: string) {
+        console.log("call" + msg + "\n");
+    }
+
+    // 記録する
+    public recordLog(msg: string) {
+        console.log("record ... " + msg + "\n");
+    }
+
+}
+
+let names: string[] = ["金庫使用ボタン", "非常ベル使用ボタン", "通常通話ボタン", "終了ボタン"];
+let nums: number[] = [1, 2, 3, 4];
+let buttonNum: number = nums.length;
+for (let i: number = 0; i < buttonNum; i++) {
+    let button: HTMLButtonElement = <HTMLButtonElement>(
+        document.createElement("button")
+    );
+    button.textContent = `${names[i]}`;
+    button.onclick = function () {
+        let temp: string = `${names[i]}`;
+        console.log("押した: " + temp);
+        safeFrame.pushButton(Number(`${nums[i]}`));
+    };
+    document.body.appendChild(button);
+    let divider: HTMLElement = <HTMLElement>document.createElement("span");
+    divider.textContent = " ";
+    document.body.appendChild(divider);
+    if (i === buttonNum - 1) {
+        let newLine: HTMLElement = <HTMLElement>document.createElement("br");
+        document.body.appendChild(newLine);
+    }
+}
+
+// npm i -D @types/jqueyで型判別出来るようにしておく（$でエラーが出るので）
+$("#hour_input").change(function () {
+    console.log("時間変更しました");
+    safeFrame.setClock();
+});
+
+// テスト実行
+let safeFrame: SafeFrame = new SafeFrame();
+console.log("safeFrame:");
+console.log(safeFrame);
+safeFrame.SefeFrame("test start");
+
